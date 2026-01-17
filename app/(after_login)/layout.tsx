a5446e11fbece8ea0363da2d51348796fec2695b
chanki <chanki.kim89@gmail.com>
2026-01-18 00:44:42 +0900

    fix: increase bottom padding in AfterLoginLayout

--- app/(after_login)/layout.tsx
+++ app/(after_login)/layout.tsx
@@ -5,7 +5,7 @@ import RootHeader from "@/components/RootHeader";
 
 export default function AfterLoginLayout({ children }: PropsWithChildren) {
   return (
-    <div className="pb-20">
+    <div className="pb-60">
       <RootHeader />
       {children}
 
